a3059e6307d1136610a5b78befe5793188549ce2
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-10 13:39:01 +0300

    same reason

--- haskell/pe1.hs
+++ haskell/pe1.hs
@@ -2,5 +2,4 @@ main :: IO ()
 validateValues :: [Int] -> [Int]
 validateValues = filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0)
 main = do
-  let list = validateValues [0 .. 999]
-  print (sum list)
+  print (sum (validateValues [0 .. 999]))
