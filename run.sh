3bb8e0a3013dd02014cf709833dbb4101093b9cf
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-10 18:16:08 +0300

    directly executing haskell files

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-out_dir="out"
-
 lang_dirs=("haskell")
 
 git_clean() {
@@ -9,28 +7,15 @@ git_clean() {
 	git reset --hard > /dev/null 2>&1
 }
 
-compile_file() {
-	ghc "$1" -o "$2" > /dev/null 2>&1
-}
-
 execute() {
-	"$1"
+	runghc "$1"
 }
 
-i=0
 for lang in "${lang_dirs[@]}"
 do
-	res_dir="$out_dir/$lang"
-	mkdir -p "$res_dir"
-	git_clean
 	for f in ./"$lang"/*
 	do
-		out_file="./$res_dir/pe$i"
-		i=$((i + 1))
-		compile_file "$f" "$out_file"
-		echo "$out_file : "
-		execute "$out_file"
+		execute "$f"
 	done
 done
-git_clean
 
