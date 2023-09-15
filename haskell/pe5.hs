09ff8c59e5b8a72a659bace2e4bbe7a923f157f4
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-16 01:54:50 +0300

    pe5.hs

--- haskell/pe5.hs
+++ haskell/pe5.hs
@@ -6,13 +6,10 @@ main :: IO ()
 
 nums = [2..20]
 
-prunedNums = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+prunedNums = [11..20]
 
 divisible :: Int -> Bool
 divisible n = all (\x -> n `mod` x == 0) prunedNums
 
 main = do
-    let prod = product nums
-    let decreasing = [prod, prod-1..20]
-    let list = [20, 40..prod]
-    print (filter divisible list)
+    print (find divisible [20, 40..product nums])
