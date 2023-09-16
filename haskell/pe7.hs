d20b0269503e4a7fbb3d483f5750be36a51edab8
Ivan Dimitrov <ivan@idimitrov.dev>
2023-09-16 15:51:53 +0300

    pe7.hs

---
+++ haskell/pe7.hs
@@ -0,0 +1,14 @@
+#!/usr/bin/env runghc
+
+import Data.List
+
+main :: IO ()
+
+primes :: [Int]
+primes = 2 : filter isPrime [3, 5..]
+
+isPrime :: Int -> Bool
+isPrime n = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p * p <= n) primes)
+
+main = do
+    print (take 10001 primes)
